f6eed60c8eb131d212ecd18575f19aa010d27d5d
Moisz22 <payasohackero96@gmail.com>
2022-08-10 19:49:37 -0500

    recursos siams

--- routes/web.php
+++ routes/web.php
@@ -48,5 +48,6 @@ Route::group(['middleware' => ['web', 'custom_auth']], function(){
 //administracion modulo
 Route::group(['middleware' => ['web', 'custom_auth']], function(){
     Route::get('/arquitectura_asociacion', 'AdministracionController@asociacion')->name('arquitectura_asociacion');
+    Route::get('/monitoreo_siams', 'MonitoreoController@index')->name('monitoreo_siams');
 
 });
